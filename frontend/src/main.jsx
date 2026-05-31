f21ae4d181cfc74989c46b193ea4575cbe3b8620
SGIR Kinshasa <admin@rtn.cd>
2026-06-01 01:33:34 +0200

    feat: sie final

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,16 +1,4 @@
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import ComingSoon from './pages/ComingSoon'
-import App from './App'
-import './index.css'
-
-const isLive = window.location.hostname === 'nzela.cd' || window.location.hostname === 'www.nzela.cd'
-
-createRoot(document.getElementById('root')).render(
-  isLive ? <ComingSoon /> :
-  <BrowserRouter>
-    <Routes>
-      <Route path="/*" element={<App />} />
-    </Routes>
-  </BrowserRouter>
-)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>);
