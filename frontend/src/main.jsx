38e230eefb98000cf6cf53dc55d7380606edb4f5
SGIR Kinshasa <admin@rtn.cd>
2026-06-01 01:38:11 +0200

    feat: site final

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>);
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import App from './App'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/*" element={<App />} />
+    </Routes>
+  </BrowserRouter>
+)
